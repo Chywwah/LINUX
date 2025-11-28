ec914b1cbc02fb2048dd7a93aa533aed8caad31b
Chywwah <honoreechywwah@gmail.com>
2025-11-28 22:30:46 +0300

    Add files via upload

---
+++ client.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Xcript a executer au niveau du client local souhaitant utiliser le serveur local
+if [ -z "$1" ];then
+	echo "./repo.sh <IP _SERVEUR>"
+	exit
+fi
+ 
+if ! grep -q "http://${1}/" /etc/apt/sources.list;then
+	echo "deb [trusted=yes] http://${1}/ ./" >> /etc/apt/sources.list
+	apt update
+fi
